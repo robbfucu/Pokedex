b061112e3d71c0697451108dd3e8b163efe6522a
Roberto Fucugauchi <robbfucu@gmail.com>
2024-04-12 11:50:30 -0600

    displayed first gen list

--- App.tsx
+++ App.tsx
@@ -1,118 +1,56 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
+    FlatList
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const pokePath = "https://pokeapi.co/api/v2/"
+const pokeQuery = "pokemon?limit=151&offset=0"
+const firstGenPokemonPath = `${pokePath}${pokeQuery}`
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+export default function App() {
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+    const [firstGenPokemonDetails, setfirstGenPokemonDetails] = useState([])
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+    useEffect(() => {
+        const fetchFirstGenPokemon = async () => {
+            const firstGenPokemonIdsResponse = await fetch(firstGenPokemonPath)
+            const firstGenPokemonIdsBody = await firstGenPokemonIdsResponse.json()
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+            const firstGenPokemonDetails = await Promise.all(
+                firstGenPokemonIdsBody.results.map(async (p) => {
+                    const pDetails = await fetch(p.url)
+                    return await pDetails.json()
+                })
+            )
+            console.log("Detalles: ", firstGenPokemonDetails)
+            setfirstGenPokemonDetails(firstGenPokemonDetails)
+        }
+        fetchFirstGenPokemon()
+    }, [])
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={firstGenPokemonDetails}
+                renderItem={({ item }) => <Text style={styles.text}> {item.name}</Text>}
+            />
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    );
 }
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+    container: {
+        flex: 1,
+        backgroundColor: "#FFF",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    text: {
+        color: "#000",
+        fontSize: 20
+    }
+})
